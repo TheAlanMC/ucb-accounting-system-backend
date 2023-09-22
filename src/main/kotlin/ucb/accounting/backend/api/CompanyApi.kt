a7f0f89476f7cc0e961ec8ef448f8131ec8c7ac0
IsaiDiaz <jsoza057@gmail.com>
2023-09-22 18:07:05 -0400

    Added endpoint to update a company

--- src/main/kotlin/ucb/accounting/backend/api/CompanyApi.kt
+++ src/main/kotlin/ucb/accounting/backend/api/CompanyApi.kt
@@ -6,8 +6,10 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ucb.accounting.backend.bl.CompanyBl
 import ucb.accounting.backend.dto.CompanyDto
@@ -48,5 +50,19 @@ class CompanyApi @Autowired constructor(private val companyBl: CompanyBl){
         val responseInfo = ResponseCodeUtil.getResponseInfo(code)
         logger.info("Code: $code - ${responseInfo.message}")
         return ResponseEntity(ResponseDto(code, responseInfo.message!!, null), responseInfo.httpStatus)
+    }
+
+    @PutMapping("/{companyId}")
+    fun putCompanyInfo(
+        @RequestParam("companyId") companyId: Long,
+        @RequestBody companyDto: CompanyDto,
+    ) : ResponseEntity<ResponseDto<CompanyDto>>{
+        logger.info("Starting the API call to put company info")
+        logger.info("PUT /api/v1/companies/{companyId}")
+        val updatedCompany = companyBl.updateCompany(companyDto, companyId)
+        val code = "200-06"
+        val responseInfo = ResponseCodeUtil.getResponseInfo(code)
+        logger.info("Code: $code - ${responseInfo.message}")
+        return ResponseEntity(ResponseDto(code, responseInfo.message!!, updatedCompany), responseInfo.httpStatus)
     }
 }
--- src/main/kotlin/ucb/accounting/backend/bl/CompanyBl.kt
+++ src/main/kotlin/ucb/accounting/backend/bl/CompanyBl.kt
@@ -78,6 +78,58 @@ class CompanyBl @Autowired constructor(
         // TODO: Change this to the s3 object id
         companyEntity.s3CompanyLogo = 1//savedS3Object.s3ObjectId.toInt()
         val savedCompany = companyRepository.save(companyEntity)
+        logger.info("Company saved successfully")
+    }
 
+    fun updateCompany (companyDto: CompanyDto, companyId: Long): CompanyDto {
+        logger.info("Starting the BL call to put company info")
+
+        industryRepository.findByIndustryIdAndStatusTrue(companyDto.industry.industryId)
+            ?: throw UasException("404-03")
+
+        businessEntityRepository.findByBusinessEntityIdAndStatusTrue(companyDto.businessEntity.businessEntityId)
+                ?: throw UasException("404-04")
+
+        val company = companyRepository.findByCompanyIdAndStatusTrue(companyId) ?: throw UasException("404-05")
+
+        // Validation that company fields are not null
+        if (companyDto.companyName.isEmpty()) throw UasException("400-06")
+        if (companyDto.companyNit.isEmpty()) throw UasException("400-06")
+        if (companyDto.companyAddress.isEmpty()) throw UasException("400-06")
+        if (companyDto.phoneNumber.isEmpty()) throw UasException("400-06")
+        if (companyDto.companyLogo.isEmpty()) throw UasException("400-06")
+
+        /*val kcUuid = KeycloakSecurityContextHolder.getSubject()!!
+
+        logger.info("User $kcUuid is updating a company")
+        // Validation of user belongs to company
+        kcUserCompanyRepository.findAllByKcUser_KcUuidAndCompany_CompanyIdAndStatusIsTrue(kcUuid, companyId)
+            ?: throw UasException("403-04")*/
+
+        /*logger.info("Saving s3 object")
+        val s3ObjectEntity = S3Object()
+        s3ObjectEntity.filename = "companyLogo"
+        s3ObjectEntity.bucket = "ucb-accounting"
+        s3ObjectEntity.filename = companyDto.companyLogo
+        val savedS3Object = s3ObjectRepository.save(s3ObjectEntity)*/
+
+        logger.info("Updating company")
+
+        company.industryId = companyDto.industry.industryId.toInt()
+        company.businessEntityId = companyDto.businessEntity.businessEntityId.toInt()
+        company.companyName = companyDto.companyName
+        company.companyNit = companyDto.companyNit
+        company.companyAddress = companyDto.companyAddress
+        company.phoneNumber = companyDto.phoneNumber
+        //TODO: Change this to the s3 object id
+        company.s3CompanyLogo = 1//savedS3Object.s3ObjectId.toInt()
+
+        val updatedCompany = companyRepository.save(company)
+
+        val s3Object: S3Object = s3ObjectRepository.findByS3ObjectIdAndStatusIsTrue(company.s3CompanyLogo.toLong())!!
+        val preSignedUrl: String = minioService.getPreSignedUrl(s3Object.bucket, s3Object.filename)
+
+        logger.info("Company updated successfully")
+        return CompanyMapper.entityToDto(updatedCompany, preSignedUrl)
     }
 }
